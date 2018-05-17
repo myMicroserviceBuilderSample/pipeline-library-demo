14c8edeb0fc1031b5ca8c881be98d88103f7788d
wofish2 <donghaiy@jp.ibm.com>
2018-05-17 19:54:58 +0900

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -4,3 +4,24 @@ def call(String name = 'human') {
   echo "Hello, ${name}."
 }
 
+// vars/timedCommand.groovy
+def setCommand(commandToRun) {
+    cmd = commandToRun
+}
+
+def getCommand() {
+    cmd
+}
+
+def runCommand() {
+   timestamps {
+      cmdOut = sh (script:"${cmd}", returnStdout:true).trim()
+   }
+}
+
+def getOutput() {
+   cmdOut
+}
+
+
+
